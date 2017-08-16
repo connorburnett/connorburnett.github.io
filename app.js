8d060883ca918008d9acaa0bb870b13de6e524f3
Connor Burnett <conb419@gmail.com>
2017-08-15 19:27:39 -0600

    night 8 15 17

--- app.js
+++ app.js
@@ -21,3 +21,39 @@ $(document).ready(function () {
         }, 2000);
     })
 })
+
+$("#box1").hover(function () {
+    $("#por1").css("display", "block");
+}, function () {
+    $("#por1").css("display", "none");
+});
+
+$("#box2").hover(function () {
+    $("#por2").css("display", "block");
+}, function () {
+    $("#por2").css("display", "none");
+});
+
+$("#box3").hover(function () {
+    $("#por3").css("display", "block");
+}, function () {
+    $("#por3").css("display", "none");
+});
+
+$("#box4").hover(function () {
+    $("#por4").css("display", "block");
+}, function () {
+    $("#por4").css("display", "none");
+});
+
+$("#box5").hover(function () {
+    $("#por5").css("display", "block");
+}, function () {
+    $("#por5").css("display", "none");
+});
+
+$("#box6").hover(function () {
+    $("#por6").css("display", "block");
+}, function () {
+    $("#por6").css("display", "none");
+});
